23b2a02847698147382ff4431ff1702aa1d69bbd
luan.reis <luan.reis@b2mlportal.com.br>
2022-12-27 19:31:18 -0300

    primeiro commit projeto-react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from "./components/banner/banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner></Banner>
+      <h1>Olá mundo!</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
         <a
           className="App-link"
